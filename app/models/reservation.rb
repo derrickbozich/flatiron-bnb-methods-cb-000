a35321ae8af6b65b2ddcb56fcc3541a1a10324cd
derrick b <dbozich1@gmail.com>
2018-08-23 20:02:45 +0000

    automatically backed up by learn

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,5 +2,26 @@ class Reservation < ActiveRecord::Base
   belongs_to :listing
   belongs_to :guest, :class_name => "User"
   has_one :review
+  
+
+
+  validates :checkin, presence: true
+  validates :checkout, presence: true
+  validate :host_is_not_guest
+
+
+  def duration
+    self.checkout - self.checkin
+  end
+
+  def total_price
+    self.listing.price * duration
+  end
+
+  def host_is_not_guest
+    guest.id
+    binding.pry
+  end
+
 
 end
